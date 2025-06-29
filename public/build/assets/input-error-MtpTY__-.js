b1d284ee64a37a51ef536ca67e436357f0953471
AndersonBrasil <andersonbrasil72@gmail.com>
2025-06-29 19:59:28 -0300

    Push

--- public/build/assets/input-error-MtpTY__-.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-D89z5eKY.js";import{c as o}from"./button-Chc8BE4H.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
